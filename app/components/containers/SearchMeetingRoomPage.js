730afc9706be37ec918bca596a91d391f58395f5
Christian Rasmussen <christian.rasmussen@me.com>
2017-10-02 20:06:28 +0200

    Initial implementation of search meeting room

--- app/components/containers/SearchMeetingRoomPage.js
+++ app/components/containers/SearchMeetingRoomPage.js
@@ -2,9 +2,17 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import {
   StyleSheet,
+  ScrollView,
   View,
+  TextInput,
   Button,
+  KeyboardAvoidingView,
 } from 'react-native';
+import {
+  TableView,
+  Section,
+} from 'react-native-tableview-simple';
+import BasicCell from '../atoms/BasicCell';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
 
@@ -12,19 +20,23 @@ import * as actions from '../../actions';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: Section.defaultProps.sectionTintColor,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  searchBarContainer: {
+    padding: 6,
+    backgroundColor: 'gray',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  searchBar: { // TODO: Temp
+    backgroundColor: 'white',
+    paddingVertical: 5,
+    paddingHorizontal: 8,
+    borderRadius: 5,
+  },
+  keyboardAvoidingView: {
+    flex: 1,
+  },
+  scrollView: {
+    flex: 1,
   },
 });
 
@@ -39,10 +51,25 @@ class SearchMeetingRoomPage extends Component {
   };
 
   render() {
+    const titlebarHeight = 64;
+
     return (
       <View style={styles.container}>
-        <Button title="Search meeting room: L123" onPress={() => this.props.searchMeetingRoom('L123')} />
-        <Button title="Search meeting room: L567" onPress={() => this.props.searchMeetingRoom('L567')} />
+        <View style={styles.searchBarContainer}>
+          <TextInput autoFocus placeholder="Search for meeting room" onChangeText={(text) => {}} style={styles.searchBar} />
+        </View>
+        <KeyboardAvoidingView behavior="padding" keyboardVerticalOffset={titlebarHeight} style={styles.keyboardAvoidingView}>
+          <ScrollView keyboardShouldPersistTaps="handled" style={styles.scrollView}>
+            <TableView>
+              <Section sectionPaddingTop={0} sectionPaddingBottom={0}>
+                <BasicCell title="Search meeting room" onPress={() => this.props.searchMeetingRoom('SEARCHED')} accessory="DisclosureIndicator" />
+                <BasicCell title="Last" onPress={() => console.log('test')} accessory="DisclosureIndicator" />
+              </Section>
+            </TableView>
+            <Button title="Search meeting room: L123" onPress={() => this.props.searchMeetingRoom('L123')} />
+            <Button title="Search meeting room: L567" onPress={() => this.props.searchMeetingRoom('L567')} />
+          </ScrollView>
+        </KeyboardAvoidingView>
       </View>
     );
   }
