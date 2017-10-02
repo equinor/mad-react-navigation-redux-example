ebfd60f63998674ca5f33e858fa3a6ff60cceeb6
Christian Rasmussen <christian.rasmussen@me.com>
2017-10-02 22:40:17 +0200

    Fixed remaining issues with DefaultPage

--- app/components/containers/DefaultPage.js
+++ app/components/containers/DefaultPage.js
@@ -35,40 +35,49 @@ class DefaultPage extends Component {
   };
 
   render() {
-    const separatorInsetLeft = 54;
+    const separatorInsetLeft = (15 * 2) + 29; // margin * 2 + iconWidth
+    const marginRight = 15;
 
     return (
       <ScrollView style={styles.scrollView}>
         <TableView>
-          <Section header="MEETING ROOM" separatorInsetLeft={separatorInsetLeft}>
+          <Section
+            separatorInsetLeft={separatorInsetLeft}
+            header="REPORT MEETING ROOM"
+            footer="Quickly report incidents related to IT equipment in meeting rooms."
+          >
             <BasicCell
-              cellImageView={<RoundedSquareIcon backgroundColor={colors.PINK} icon={<Icon name="ios-pin" size={17} color="white" />} style={{ marginRight: 10 }} />}
+              cellImageView={<RoundedSquareIcon backgroundColor={colors.PINK} icon={<Icon name="ios-pin" size={17} color="white" />} style={{ marginRight }} />}
               title="Search meeting room"
               onPress={() => this.props.dispatch(actions.goToMeetingRoomSearch())}
               accessory="DisclosureIndicator"
             />
             <BasicCell
-              cellImageView={<RoundedSquareIcon backgroundColor={colors.PINK} icon={<Icon name="md-camera" size={17} color="white" />} style={{ marginRight: 10 }} />}
+              cellImageView={<RoundedSquareIcon backgroundColor={colors.PINK} icon={<Icon name="md-camera" size={17} color="white" />} style={{ marginRight }} />}
               title="Scan yellow label"
               onPress={() => this.props.dispatch(actions.goToMeetingRoomScanLabel())}
               accessory="DisclosureIndicator"
             />
             <BasicCell
-              cellImageView={<RoundedSquareIcon backgroundColor={colors.PINK} icon={<MIcon name="label-outline" size={17} color="white" />} style={{ marginRight: 10 }} />}
+              cellImageView={<RoundedSquareIcon backgroundColor={colors.PINK} icon={<MIcon name="label-outline" size={17} color="white" />} style={{ marginRight }} />}
               title="Look up yellow label"
               onPress={() => this.props.dispatch(actions.goToMeetingRoomLookupLabel())}
               accessory="DisclosureIndicator"
             />
           </Section>
-          <Section header="EQUIPMENT" separatorInsetLeft={separatorInsetLeft}>
+          <Section
+            separatorInsetLeft={separatorInsetLeft}
+            header="REPORT IT EQUIPMENT"
+            footer="Quickly report incidents related to IT equipment."
+          >
             <BasicCell
-              cellImageView={<RoundedSquareIcon backgroundColor={colors.PINK} icon={<Icon name="md-camera" size={17} color="white" />} style={{ marginRight: 10 }} />}
+              cellImageView={<RoundedSquareIcon backgroundColor={colors.PINK} icon={<Icon name="md-camera" size={17} color="white" />} style={{ marginRight }} />}
               title="Scan yellow label"
               onPress={() => this.props.dispatch(actions.goToEquipmentScanLabel())}
               accessory="DisclosureIndicator"
             />
             <BasicCell
-              cellImageView={<RoundedSquareIcon backgroundColor={colors.PINK} icon={<MIcon name="label-outline" size={17} color="white" />} style={{ marginRight: 10 }} />}
+              cellImageView={<RoundedSquareIcon backgroundColor={colors.PINK} icon={<MIcon name="label-outline" size={17} color="white" />} style={{ marginRight }} />}
               title="Look up yellow label"
               onPress={() => this.props.dispatch(actions.goToEquipmentLookupLabel())}
               accessory="DisclosureIndicator"
